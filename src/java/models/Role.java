7f6a06f8156cf20d83f9298e8eb4dbbb56d4bf02
807930 <807930@SAIT225240IT.ACDM.DS.SAIT.CA>
2021-03-20 13:53:12 -0600

    Copied Lab 8 code for Lab 9

---
+++ src/java/models/Role.java
@@ -0,0 +1,39 @@
+
+package models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 807930
+ */
+public class Role implements Serializable {
+    //private int roleID;
+    private int roleID;
+    private String roleName;
+    
+    public Role() {
+        
+    }
+    
+    public Role(int roleID, String roleName) {
+        this.roleID = roleID;
+        this.roleName = roleName;
+    }
+    
+    public int getRoleID(){
+        return roleID;
+    }
+    
+    public void setRoleID(int roleID){
+        this.roleID = roleID;
+    }
+    
+    public String getRoleName() {
+        return roleName;
+    }
+    
+    public void setRoleName(String roleName){
+        this.roleName = roleName;
+    }
+}
---
+++ src/java/services/RoleService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import dataaccess.RoleDB;
+import java.util.List;
+import models.Role;
+
+/**
+ *
+ * @author 807930
+ */
+public class RoleService {
+    
+        public List<Role> getAll(Role role) throws Exception {
+        RoleDB roleDB = new RoleDB();
+        List<Role> roles = roleDB.getAll(role);
+        return roles;
+    }
+}
